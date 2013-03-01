763ef7ba37d041801b4a843810c0623ff9f460b5
Hua Liang <h.liang89@hotmail.com>
2013-03-01 00:32:19 -0800

    initial MvcWidget commit

---
+++ MvcWidget1/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWidget1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my <b>default</b> response.";
+        }
+
+        public string Welcome()
+        {
+            return "Welcome to <b>Hello World<b/> response.";
+        }
+
+
+    }
+}
---
+++ MvcWidget1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcWidget1.MvcApplication" Language="C#" %>
