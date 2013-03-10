92339e5ea89fb5c70be38c6bdea4db4e1b075cd0
Hua Liang <h.liang89@hotmail.com>
2013-03-09 17:29:59 -0800

    added About page enrollment date statstics

--- ContosoMvcApp/Controllers/HomeController.cs
+++ ContosoMvcApp/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ContosoMvcApp.Models;
+using ContosoMvcApp.ViewModels;
 
 namespace ContosoMvcApp.Controllers
 {
     public class HomeController : Controller
     {
+        private SchoolDBContext db = new SchoolDBContext();
+
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to Contoso University!";
@@ -17,9 +21,15 @@ namespace ContosoMvcApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "On this site you can access University course access system";
+            var data = from s in db.Students
+                       group s by s.EnrollmentDate into dateGroup
+                       select new EnrollmentDateGroup()
+                       {
+                           EnrollmentDate = dateGroup.Key,
+                           StudentCount = dateGroup.Count()
+                       };
 
-            return View();
+            return View(data);
         }
 
         public ActionResult Contact()
---
+++ ContosoMvcApp/ViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoMvcApp.ViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        [Display(Name="Enrollment Date")]
+        [DataType(DataType.Date)]
+        public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name="Student Count")]
+        public int StudentCount { get; set; }
+    }
+}
